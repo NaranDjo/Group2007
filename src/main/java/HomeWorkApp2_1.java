e43b46ca1199bfb138a6b7e754956b4c7724b11b
Naran <narandordzhiev1993@gmail.com>
2022-01-02 19:43:51 +0400

    Второе домашнее задание Неполное

---
+++ src/main/java/HomeWorkApp2_1.java
@@ -0,0 +1,12 @@
+public class HomeWorkApp2_1 {
+
+    public static boolean numbersSum(int x1, int x2) {
+        int sum = x1 + x2;
+        return (sum >= 10 && sum <= 20);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("sum");
+
+    }
+}
---
+++ src/main/java/HomeWorkApp2_4.java
@@ -0,0 +1,8 @@
+public class HomeWorkApp2_4 {
+    public static void printWordNTimes(String word, int i) {
+        for (i = 0; i < 10; i++) {
+            System.out.println("word" + i);
+
+        }
+    }
+}
